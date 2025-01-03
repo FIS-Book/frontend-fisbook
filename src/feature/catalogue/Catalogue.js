03b529b43b2076147f2f732cd5f58e26438fd59f
Sandra Jaramillo Dorado <186032883+SandraJD2@users.noreply.github.com>
2025-01-03 19:59:06 +0100

    feat: Add search styles, filters and buttons on catalogue and book detail pages

--- src/feature/catalogue/Catalogue.js
+++ src/feature/catalogue/Catalogue.js
@@ -9,8 +9,19 @@ function Catalogue() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [searchTerm, setSearchTerm] = useState('');
+  const [filterBy, setFilterBy] = useState('title');
   const navigate = useNavigate();
 
+  // Mapeo idioma
+  const languageMap = {
+    en: 'Inglés',
+    es: 'Español',
+    fr: 'Francés',
+    de: 'Alemán',
+    it: 'Italiano',
+    pt: 'Portugués',
+  };
+  
   // Función para obtener los libros desde el backend
   const fetchBooks = async () => {
     try {
@@ -34,17 +45,39 @@ function Catalogue() {
   const handleSearchChange = (e) => {
     const term = e.target.value;
     setSearchTerm(term); // Actualiza el término de búsqueda
-    // Filtra los libros por título o autor según el término de búsqueda
+
     if (term === '') {
       setFilteredBooks(books); // Si la barra está vacía, muestra todos los libros
     } else {
       setFilteredBooks(
-        books.filter((book) =>
-          book.title.toLowerCase().includes(term.toLowerCase()) ||
-          book.author.toLowerCase().includes(term.toLowerCase())
-        )
+        books.filter((book) => {
+          if (filterBy === 'categories') {
+            return book.categories.some(category => category.toLowerCase().includes(term.toLowerCase()));
+          }
+          if (filterBy === 'publicationYear') {
+            return book.publicationYear?.toString().includes(term);
+          }
+          if (filterBy === 'language') {
+            return languageMap[book.language].toLowerCase().includes(term.toLowerCase());
+          }
+          return (
+            book[filterBy] && 
+            book[filterBy].toLowerCase().includes(term.toLowerCase())
+          );
+        })
       );
     }
+  };
+
+  // Manejo del evento de click en el botón
+  const handleViewDetails = (book) => {
+    const bookDetailsUrl = `/book-details/${book.isbn}`;
+    navigate(bookDetailsUrl);
+  };
+
+  // Manejo del evento para volver al HomePage
+  const handleGoToHome = () => {
+    navigate('/'); // Ruta del HomePage
   };
 
   // Renderización condicional según el estado de carga o error
@@ -56,25 +89,43 @@ function Catalogue() {
     return <p>{error}</p>;
   }
 
-  // Manejo del evento de click en el botón
-  const handleViewDetails = (book) => {
-    const bookDetailsUrl = `/book-details/${book.isbn}`;
-    navigate(bookDetailsUrl);
-  };
-
   // Renderizar libros cuando estén disponibles
   return (
     <div className="catalogue-container">
       <h2>Catálogo de Libros</h2>
 
+      {/* Botón para volver al HomePage */}
+      <button onClick={handleGoToHome} className="btn btn-home">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
+          <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+        </svg>
+        Home
+      </button>
+
       {/* Barra de búsqueda */}
       <div className="search-bar">
+        <select
+          value={filterBy}
+          onChange={(e) => setFilterBy(e.target.value)}
+          className="form-control filter-dropdown"
+        >
+          <option value="title">Título</option>
+          <option value="author">Autor</option>
+          <option value="publicationYear">Año</option>
+          <option value="language">Idioma</option>
+          <option value="isbn">ISBN</option>
+          <option value="categories">Categoría</option>
+        </select>
         <input
           type="text"
-          placeholder="Buscar por título o autor..."
+          placeholder={
+            filterBy === 'language'
+              ? 'Buscar por idioma (Ej. Inglés, Español...)'
+              : `Buscar por ${filterBy}...`
+          }
           value={searchTerm}
           onChange={handleSearchChange} // Actualiza el término de búsqueda
-          className="form-control"
+          className="form-control search-input"
         />
       </div>
 
@@ -92,6 +143,9 @@ function Catalogue() {
                 <h4>{book.title}</h4>
                 <p>{book.author}</p>
                 <button onClick={() => handleViewDetails(book)} className="btn btn-primary">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                  </svg>
                   Ver detalle
                 </button>
               </div>
--- src/feature/catalogue/HomePage.js
+++ src/feature/catalogue/HomePage.js
@@ -49,6 +49,11 @@ function HomePage() {
     fetchLatestBooks();
   }, []); // Solo se ejecuta una vez
 
+   // Función para manejar el click en un libro
+   const handleBookClick = (isbn) => {
+    navigate(`/book-details/${isbn}`);  
+  };
+
   // Categorizar los libros destacados por `featuredType`
   const bestsellers = featuredBooks.filter(book => book.featuredType === 'bestSeller');
   const awardWinner = featuredBooks.filter(book => book.featuredType === 'awardWinner');
@@ -62,6 +67,9 @@ function HomePage() {
                 className="button-catalogue"
                 onClick={() => navigate('/catalogue')}
             >
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-book" viewBox="0 0 16 16">
+                <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
+              </svg>
             Ver catálogo
             </button>
         </div>
@@ -89,7 +97,7 @@ function HomePage() {
           {bestsellers.length > 0 ? (
             <div className="featured-books">
               {bestsellers.map((book) => (
-                <div key={book.isbn} className="book-item">
+                <div key={book.isbn} className="book-item" onClick={() => handleBookClick(book.isbn)}>
                   <img 
                     src={book.coverImage} 
                     alt={`Cover of ${book.title}`} 
@@ -113,7 +121,7 @@ function HomePage() {
           {awardWinner.length > 0 ? (
             <div className="featured-books">
               {awardWinner.map((book) => (
-                <div key={book.isbn} className="book-item">
+                <div key={book.isbn} className="book-item" onClick={() => handleBookClick(book.isbn)}>
                   <img 
                     src={book.coverImage} 
                     alt={`Cover of ${book.title}`} 
@@ -137,7 +145,7 @@ function HomePage() {
           {latestBooks.length > 0 ? (
             <div className="featured-books">
               {latestBooks.map((book) => (
-                <div key={book.isbn} className="book-item">
+                <div key={book.isbn} className="book-item" onClick={() => handleBookClick(book.isbn)}>
                   <img 
                     src={book.coverImage} 
                     alt={`Cover of ${book.title}`} 
